eb7344b61999225076d790db9668d064442d0708
Nikitha Rajkumar <nikitharajkumar18@gmail.com>
2025-12-08 08:51:33 +0530

    day 5 stepped into javascript

--- src/promise/promise-component.jsx
+++ src/promise/promise-component.jsx
@@ -1,69 +1,42 @@
 import React from "react";
-import { PromiseStyle } from "./promise-component-style";
-const PromiseComponent = () => {
+import { OurPromisesStyle } from "./our-promises-style";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { OurpromisesData } from "./data";
+
+const OurPromisesComponent = () => {
   return (
-    <PromiseStyle>
-      <div>
-        <div className="title-subtitle">
-          <h2 className="title">Our Promise</h2>
-          <p className="subtitle">
-            As part of our high quality service, we'd like to offer something
-            extra too.
-          </p>
-        </div>
-        <div className="element">
-          <div className="sub-element">
-            <div className="item">Top code Quality</div>
-            <div className="item-content">
-              Our commitment goes beyond mere code - it encompasses providing
-              solutions. You receive W3C fully compliant markup, without any
-              compromise on quality.
-            </div>
-          </div>
-          <div className="sub-element">
-            <div className="item">Responsive</div>
-            <div className="item-content">
-              We understand the value of a positive attitude, timely
-              responsiveness, and adaptability. We're dedicated to attentively
-              listening and leveraging our utmost capabilities to serve you.
-            </div>
-          </div>
-          <div className="sub-element">
-            <div className="item">Rockstar team</div>
-            <div className="item-content">
-              Our expertise is our unique selling proposition. You'll have
-              access to an all-star team of experienced professionals fully
-              dedicated to serving you, armed with coding skills that are truly
-              exceptional.
-            </div>
-          </div>
-          <div className="sub-element">
-            <div className="item">Fast turn arounds</div>
-            <div className="item-content">
-              The importance of speed is on par with quality for us. We are
-              swift, nimble, and capable of providing high-quality code within a
-              timeframe that suits your preferences.
-            </div>
-          </div>
-          <div className="sub-element">
-            <div className="item">Life-time Support</div>
-            <div className="item-content">
-              Our commitment to code quality is exemplified by our enduring
-              support. If any bugs happen to emerge in the future, feel free to
-              reach out to us for assistance.
-            </div>
-          </div>
-          <div className="sub-element">
-            <div className="item">Secured Agreement</div>
-            <div className="item-content">
-              The creative work you receive is entirely your own, and we
-              unequivocally respect that. We assure you of our commitment and
-              readiness to formalize this with a Non-Disclosure Agreement (NDA).
-            </div>
-          </div>
-        </div>
+    <OurPromisesStyle>
+      <div className="paras">
+        <h1>Our Promise</h1>
+        <p>
+          As part of our high quality service, we'd like to offer something
+          extra too.
+        </p>
       </div>
-    </PromiseStyle>
+      <div className="parent">
+        {OurpromisesData.map((item, index) => {
+          return (
+            <div
+              className="child"
+              key={index}
+              style={{ borderTop: `4px solid ${item.color}` }}
+            >
+              <div className="title__parent">
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  color={item.color}
+                  fontSize="30px"
+                />
+                <h1 className="title">{item.title}</h1>
+              </div>
+              <div>
+                <p className="para">{item.content}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </OurPromisesStyle>
   );
 };
-export default PromiseComponent;
+export default OurPromisesComponent;
