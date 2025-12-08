8fd3d4122a70bba1965666930298f9ddfd2daf7c
Nikitha Rajkumar <nikitharajkumar18@gmail.com>
2025-12-08 11:48:23 +0530

    day 6 route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
 import { TrustedByStyle } from "./trusted-by/trusted-by-style";
 // import HelloWorldComponent from "./hello-world/hello-world-component";
 // import Form from "./hello-world/form";
@@ -9,32 +10,13 @@ import { Routes } from "react-router-dom";
 function App() {
   return (
     <>
-    {/* <HelloWorldComponent/>
-    <Form/>
-    <LoginForm/> 
-    <ImpactAtGlance/> */}
-    {/* <PromiseComponent/> */}
-    <TrustedByStyle
-    titleColor="#333"
-    numberColor="#333"
-    detailColor="#555"
-    cardBg="#f8f8f8"
-    shadow="0 4px 10px rgba(0,0,0,0.2)"
-    gap="40px"
-    cardWidth="250px"
-    cardHeight="220px"
-    >
-    <TrustedbyComponent 
-    title="Trusted By"
-    items={[
-      {number:"100+",label:"Colleges"},
-      {number:"150",label:"Professional trainers"},
-      {number:"1000+",label:"Study Materials"},
-      {number:"10000+",label:"Students"},
-    ]}
-    />
-    </TrustedByStyle>
-
+    <Routes>
+      <Route path="/eshwar" element={<TrustedByStyle/>}/>
+      <Route path="/flex-box" element={<FlexBoxComponent/>}/>
+      <Route path="/flex-wrap" element={<FlexWrapComponent/>}/>
+      <Route path="/getintouch" element={<GetInTouchComponent/>}/>
+      <Route path="/impactatglance" element={<ImpactAtGlance/>}/>
+    </Routes>
     </>
   );
 }
